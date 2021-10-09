eaaed913b04e83c38be505e95c4b793bbdd4437a
Win <win@Wins-MacBook-Pro.local>
2021-10-09 18:23:29 +0700

    menu text size

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,25 +4,17 @@ import Deployed from "../components/deployed";
 import Footer from "../components/footer"
 import Receivable from "../components/receivable";
 import CTA from "../components/cta";
-import styled from "styled-components";
 
-const Wrapper = styled.div`
-  width: auto;
-  margin: auto;
-  
-`;
 
 export default function Overview() {
   return (
     <div>
       <NavBar />
-      <Wrapper>
         <Summary />
         <CTA />
         <Deployed />
         <Receivable />
         <Footer />
-      </Wrapper>
     </div>
   );
 }
