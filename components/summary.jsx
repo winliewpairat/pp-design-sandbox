916ed6a161f6249f877a9675579a323b0a5ddc1c
Win <win@Wins-MacBook-Pro.local>
2021-10-10 22:26:02 +0700

    breakline logo mobile

--- components/summary.jsx
+++ components/summary.jsx
@@ -25,13 +25,13 @@ class Summary extends React.Component {
             <div className={styles.h3}>10 %</div>
           </div>
           <div className={styles.nested}>
-            <div className={styles.h6}>Total Deposit</div>
+            <div className={styles.h6}>Portfolio Value</div>
             <div className={styles.h3}>31,200,637 ฿</div>
           </div>
         </div>
         <div className={styles.statsRow}>
           <div className={styles.nested}>
-            <div className={styles.header}>Portfolio Value</div>
+            <div className={styles.header}>Total Deposit</div>
             <div className={styles.h6}>20,200,637 ฿</div>
           </div>
           <div className={styles.nested}>
