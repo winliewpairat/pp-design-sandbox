cabcec2d85184ba731e6067359eb06c4bca92bb3
Win <win@Wins-MacBook-Pro.local>
2021-10-10 11:53:07 +0700

    add hover tooltip

--- components/summary.jsx
+++ components/summary.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./summary.module.scss";
+import ReactTooltip from "react-tooltip";
 
 class Summary extends React.Component {
   render() {
     return (
       <div className={styles.container}>
+        <ReactTooltip />
         <div className={styles.welcome}>
           <div className={styles.h5}>👋 Welcome, Mr.Andrew Wang</div>
-          <div className={styles.h5 }>Here is your investment summary</div>
+          <div className={styles.h5}>Here is your investment summary</div>
         </div>
         <div className={styles.stats}>
           <div className={styles.nested}>
-            <div className={styles.h6}>Net Return</div>
+            <div className={styles.tool}>
+              <div className={styles.h6}>Net Return </div>
+              <div className={styles.smallText} data-multiline="true" data-tip="Net Realized Return: 10,000,000 ฿ <br/> Net Unrealized Return: 10,000,000 ฿" data-effect="solid" data-type="light"> — More Details</div>
+            </div>
             <div className={styles.h3}>20,200,637 ฿</div>
           </div>
           <div className={styles.nested}>
